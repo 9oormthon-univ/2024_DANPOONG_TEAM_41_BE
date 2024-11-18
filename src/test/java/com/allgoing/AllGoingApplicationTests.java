918eb9ad32bde2713d12a84630fef5740bbcf66c
inswal843 <inswal843@gmail.com>
2024-11-18 14:33:48 +0900

    init commit

---
+++ src/test/java/com/allgoing/AllGoingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allgoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllGoingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
