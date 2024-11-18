91bd4a3a3b6ec2c59d276fc42b3da05ba23ea24b
inswal843 <inswal843@gmail.com>
2024-11-18 18:06:15 +0900

    API서버 확인용 요청 시큐리티 예외 추가

--- src/main/java/com/allgoing/global/config/SecurityConfig.java
+++ src/main/java/com/allgoing/global/config/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/health-check").permitAll()
                         .anyRequest().authenticated()
                 )
                 .csrf(csrf -> csrf.disable()); // CSRF 비활성화
