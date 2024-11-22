66ab101f4e2df4d1cbef9057995ff6a4badc5014
inswal843 <inswal843@gmail.com>
2024-11-22 11:26:14 +0900

    [FIX] store 변경사항 반영

--- src/main/java/com/allgoing/domain/review/domain/Review.java
+++ src/main/java/com/allgoing/domain/review/domain/Review.java
@@ -48,12 +48,13 @@ public class Review extends BaseEntity {
     private List<ReviewComment> reviewComments = new ArrayList<>();
 
     @Builder
-    public Review(User user, String reviewTitle, String reviewContent){
+    public Review(User user, String reviewTitle, String reviewContent, int likeCount, String writerName, Store store) {
         this.user = user;
         this.reviewTitle = reviewTitle;
         this.reviewContent = reviewContent;
-        this.likeCount = 0;
-        this.writerName = user.getName();
+        this.likeCount = likeCount;
+        this.writerName = writerName;
+        this.store = store;
     }
 
     public void incrementLikeCount() {
